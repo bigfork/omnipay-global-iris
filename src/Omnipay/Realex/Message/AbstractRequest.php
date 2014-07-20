65107d8ba4b08d05df77ec394baf17b94f70cae4
Jamie Holdroyd <jamie@molt.in>
2014-07-20 11:14:42 +0100

    Adding base data and signature generation

--- src/Omnipay/Realex/Message/AbstractRequest.php
+++ src/Omnipay/Realex/Message/AbstractRequest.php
@@ -30,6 +30,42 @@ abstract class AbstractRequest extends \Omnipay\Common\Message\AbstractRequest
         return $this->setParameter('secret', $value);
     }
 
-    
+    public function getBaseData($autoSettle = true)
+    {
+        $this->validate('amount', 'card');
+
+        $this->getCard()->validate();
+
+        $data = array(
+            'MERCHANT_ID'      => $this->getMerchantId(),
+            'ORDER_ID'         => $this->getTransactionId(),
+            'CURRENCY'         => $this->getCurrency(),
+            'AMOUNT'           => round( $this->getAmount() * 100 ),
+            'TIMESTAMP'        => gmdate('YmdHis'),
+            'AUTO_SETTLE_FLAG' => $autoSettle
+        );
+
+        $data['SHA1HASH'] = $this->createSignature($data);
+
+        return $data;
+    }
+
+    public function createSignature($data)
+    {
+        $hash = sha1(implode('.', array(
+            gmdate('YmdHis'),
+            $data['MERCHANT_ID'],
+            $data['ORDER_ID'],
+            $data['AMOUNT'],
+            $data['CURRENCY']
+        )));
+
+        return sha1("{$hash}.".$this->getSecret());
+    }
+
+    protected function getEndpoint()
+    {
+        return $this->getTestMode() ? $this->testEndpoint : $this->liveEndpoint;
+    }
 
 }
