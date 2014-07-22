7efd87ed9c08fc3de415ea9527e5d1258f2637a2
Jamie Holdroyd <jamie@molt.in>
2014-07-22 15:59:44 +0100

    Small refactor

--- src/Omnipay/Realex/Message/RedirectAuthorizeRequest.php
+++ src/Omnipay/Realex/Message/RedirectAuthorizeRequest.php
@@ -5,12 +5,45 @@ namespace Omnipay\Realex\Message;
 use Omnipay\Common\Message\RedirectResponseInterface;
 
 /**
- * Realex Redirect Purchase Request
+ * Realex Redirect Authorize Request
  */
-class RedirectPurchaseRequest extends RedirectPurchaseRequest
+class RedirectAuthorizeRequest extends Response implements RedirectResponseInterface
 {
+    protected $liveCheckoutEndpoint = 'https://hpp.realexpayments.com/pay';
+    protected $testCheckoutEndpoint = 'https://hpp.sandbox.realexpayments.com/pay';
+
+    public function isSuccessful()
+    {
+        return false;
+    }
+
+    public function isRedirect()
+    {
+        return true;
+    }
+
+    public function getRedirectUrl()
+    {
+        return $this->getCheckoutEndpoint();
+    }
+
+    public function getTransactionReference()
+    {
+        return $this->getRequest()->getTransactionId();
+    }
+
+    public function getRedirectMethod()
+    {
+        return 'POST';
+    }
+
     public function getRedirectData()
     {
         return $this->getRequest()->getBaseData(false);
+    }
+
+    protected function getCheckoutEndpoint()
+    {
+        return $this->getRequest()->getTestMode() ? $this->testCheckoutEndpoint : $this->liveCheckoutEndpoint;
     }
 }
